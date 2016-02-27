36a9d41a9d47806f1c6bcf48fbb7939a78fa0321
kohliravin7 <kohliravin7@gmail.com>
2016-02-27 23:12:43 +0530

    Initial Commit

---
+++ FBSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FBSDKDemo
+//
+//  Created by Ravin Kohli on 27/02/16.
+//  Copyright © 2016 Ravin Kohli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
